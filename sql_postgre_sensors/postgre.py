833dafeb6ffd9ee1e178c6435e2983556ca166f0
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2020-12-07 19:34:28 +0200

    second push

--- sql_postgre_sensors/postgre.py
+++ sql_postgre_sensors/postgre.py
@@ -1,9 +1,25 @@
 import psycopg2
 import ssl
-from account import user_acc, password, port, host
+from account import user_acc, password, port, host, database
 class DbOperations:
-    def __init__(self,database):
+    def __init__(self):
         self.database = database
-        self.con = psycopg2.connect(dbname=self.database,user=user_acc,password=password,port=port,host=host,sslcert='',sslkey='',sslrootcert='')
+        self.con = psycopg2.connect(dbname=database,user=user_acc,password=password,port=port,host=host,sslcert='~/ssl/ssl-cert.crt',sslkey='~/ssl/ssl-key.key',sslrootcert='~/ssl/ca-cert.crt')
+        self.cur = self.con.cursor()
+
+
+
+    ## method to commit data to specific project 'telemetry data'
+
+    def omit_data(self,time,cpu,cam,out,hum):
+        try:
+            self.cur.execute("INSERT INTO telemetry_data VALUES (%s,%s, %s, %s, %s)",(time,cpu,cam,out,hum))
+            print('Data transfer has been executed successfully')
+        except Exception as e:
+            print('Error accured :',e)
+        finally:
+            pass
+        
+
 
 
---
+++ sql_postgre_sensors/sensor_data.py
@@ -0,0 +1,64 @@
+from gpiozero import CPUTemperature
+from postgre import DbOperations
+from datetime import datetime
+import Adafruit_DHT
+import time
+import glob
+import os
+from time import strftime
+
+
+class MainOperation:
+    def __init__(self):
+        #self.DB = DbOperations()
+        self.cpu_t = CPUTemperature()
+        self.DHT_SENSOR = Adafruit_DHT.DHT22
+        self.DHT_PIN = 18
+
+    def _get_cput(self) -> float:
+        return round(self.cpu_t.temperature, 1)
+
+    def _get_out_temp_hum(self) -> list:
+        humidity, temp = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
+        return [round(humidity, 2), round(temp, 2)]
+
+    def _get_cam_temperature(self) -> float:
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')[0]
+        device_file = device_folder + '/w1_slave'
+
+        def _read_temp_raw():
+            f = open(device_file, 'r')
+            lines = f.readlines()
+            f.close()
+            return lines
+
+        def _read_temp():
+            lines = _read_temp_raw()
+            while lines[0].strip()[-3:] != 'YES':
+                time.sleep(0.2)
+                lines = _read_temp_raw()
+            equals_pos = lines[1].find('t=')
+            if equals_pos != -1:
+                temp_string = lines[1][equals_pos+2:]
+                temp_c = float(temp_string) / 1000.0
+                temp_f = temp_c * 9.0 / 5.0 + 32.0
+                return temp_c
+        return _read_temp()
+
+
+if __name__ == '__main__':
+    op = MainOperation()
+    db = DbOperations()
+    while True:
+        _time = strftime(datetime.now(),'%H:%M:%S')
+        CPU_TEMP = op._get_cput()
+        OUT_HUM, OUT_TEMP = op._get_out_temp_hum()
+        CAM_TEMP = op._get_cam_temperature()
+
+        print('CPU temp: {}C, Camera temp: {}C, Outside temperature: {}C, Outside humidity: {}%'.format(
+            CPU_TEMP, CAM_TEMP, OUT_TEMP, OUT_HUM))
+        db.omit_data(_time,CPU_TEMP,CAM_TEMP,OUT_TEMP, OUT_HUM)
+        time.sleep(2)
